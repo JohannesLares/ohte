2ac2b783461995ca062841db798d9863b4d1dc96
Johannes Lares <johq.lares@gmail.com>
2018-11-20 15:10:31 +0200

    .gitignoren päivitys ja typo

--- Yliopistobot/src/main/java/fi/johanneslares/yliopistobot/dao/ChatStateDao.java
+++ Yliopistobot/src/main/java/fi/johanneslares/yliopistobot/dao/ChatStateDao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fi.johanneslares.ylioistobot.dao;
+package fi.johanneslares.yliopistobot.dao;
 
 import fi.johanneslares.yliopistobot.Chat;
 
