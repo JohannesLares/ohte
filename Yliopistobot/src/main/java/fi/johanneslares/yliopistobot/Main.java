a17cc70c40e0cc0a699c5d280ef6d9c4cfa169dd
Johannes Lares <johannes.lares@helsinki.fi>
2018-12-22 22:24:08 +0200

    Bot started succesfully

--- Yliopistobot/src/main/java/fi/johanneslares/yliopistobot/Main.java
+++ Yliopistobot/src/main/java/fi/johanneslares/yliopistobot/Main.java
@@ -7,6 +7,7 @@ package fi.johanneslares.yliopistobot;
 
 import fi.johanneslares.yliopistobot.threads.AutoSender;
 import fi.johanneslares.yliopistobot.threads.AutoMessager;
+import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.telegram.telegrambots.ApiContextInitializer;
@@ -19,6 +20,12 @@ public class Main {
      * @param args 
      */
     public static void main(String[] args) {
+        try {
+            checkForFiles();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(0);
+        }
         ApiContextInitializer.init();
         TelegramBotsApi api = new TelegramBotsApi();
         try {
@@ -26,8 +33,18 @@ public class Main {
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
         AutoMessager m = new AutoMessager();
         AutoSender s = new AutoSender("Eka");
+        System.out.println("Bot started successfully");
+    }
+    
+    private static void checkForFiles() throws Exception {
+        File user = new File("./user.json");
+        File chat = new File("./chatstate.json");
+        File message = new File("./MessageQueue.json");
+        File conf = new File("./bot.conf");
+        if (!user.exists() || !chat.exists() || !message.exists() || !conf.exists()) {
+            throw new Exception("Required file not found!");
+        }
     }
 }
